a898e375cb0d383c6588733cbe7bac92ab9b0efd
Wang Ge <gw980531@gmail.com>
2021-10-07 20:06:44 +0800

    Minor refactoring.

--- tools/write_mSOL_to_file.py
+++ tools/write_mSOL_to_file.py
@@ -1,10 +1,10 @@
-# bitquery
 import requests
 from datetime import datetime
 
-result = requests.get('https://intense-temple-44991.herokuapp.com/collaterals/mSOL').json()
+asset_name = "mSOL"
+result = requests.get('https://intense-temple-44991.herokuapp.com/collaterals/%s' % asset_name).json()
 date = datetime.today().strftime('%Y-%m-%d')
 
-with open("%s.txt" % date, "w") as f:
+with open("%s-%s.txt" % (asset_name, date), "w") as f:
   for merDeposit in result:
     f.writelines("%s,%s\n" % (merDeposit["owner"], merDeposit["balanceInLamport"]))
