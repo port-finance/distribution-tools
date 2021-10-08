e6388d810d321bda4d3ef4aa834ccaa1cdfc60b9
Wang Ge <gw980531@gmail.com>
2021-10-08 17:59:56 +0800

    Fixed bug in matching floating point number.

--- tools/proportional_distributor/proportional_send_mnde.py
+++ tools/proportional_distributor/proportional_send_mnde.py
@@ -33,7 +33,7 @@ env = {
 total_amount = 13736
 
 result = subprocess.run(['spl-token', 'account-info',  env['TOKEN_MINT']], stdout=subprocess.PIPE)
-token_amount = re.search("(?<=Balance: )\d", result.stdout.decode('utf-8')).group(0)
+token_amount = re.search(r"(?<=Balance: )\d+\.\d *", result.stdout.decode('utf-8')).group(0)
 if int(token_amount) < total_amount:
   print(f'Not enough token: {token_amount} required: {total_amount}')
   exit(1)
