93d28def82bb2d85280e7490e6f41755e2d1943a
Sybil <94350520+SyblePaul@users.noreply.github.com>
2021-12-16 11:07:51 +0800

    Deleted.

--- tools/proportional_distributor/periodicSend.py
+++
@@ -1,8 +0,0 @@
-from crontab import CronTab
-
-cron = CronTab(user='wangge')
-for job in cron:
-  print(job)
-job = cron.new(command='echo hello_world >> ~/output.txt')
-job.day.every(1)
-cron.write()
