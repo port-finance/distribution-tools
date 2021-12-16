5c02770220c6ac166dd0735ff2a5abcf6aed0f07
Sybil <94350520+SyblePaul@users.noreply.github.com>
2021-12-16 11:18:51 +0800

    Added date.

--- tools/proportional_distributor/periodic_send.py
+++ tools/proportional_distributor/periodic_send.py
@@ -3,6 +3,6 @@ from crontab import CronTab
 cron = CronTab(user='wangge')
 for job in cron:
   print(job)
-job = cron.new(command='echo hello_world >> ~/output.txt')
+job = cron.new(command='echo hello_world && date >> ~/output.txt')
 job.day.every(1)
 cron.write()
